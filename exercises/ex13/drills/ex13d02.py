b2ad741eb07aee9bff0a36be259cafa61caa1093
Axel Olsson <olsson.axel@gmail.com>
2018-12-07 14:26:06 +0100

    Add solution for ex13

---
+++ exercises/ex13/drills/ex13d02.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, alpha, beta = argv
+
+print("The script is called:", script)
+print("Your first variable is:", alpha)
+print("Your second variable is:", beta)
+
---
+++ exercises/ex13/drills/ex13d03.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, name = argv
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So {name}, you're {age} old, {height} tall and {weight} heavy")
